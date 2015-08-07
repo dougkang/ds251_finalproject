8d5d775831fa025402551b668410321e46f387c8
Doug Kang <dougkang@gmail.com>
2015-08-07 08:24:58 -0700

    fixing bug

--- preproc/extract_features.sh
+++ preproc/extract_features.sh
@@ -5,7 +5,7 @@ out=`echo $line | cut -d/ -f3- | sed 's/\//-/g'`
 echo "reading $line, writing to /millionSongFeats/$out"
 /usr/local/hadoop/bin/hdfs dfs -get $line /tmp/${out}.h5
 echo "extracting features from /tmp/$out"
-python preproc/msongsdb/extract_features.py /tmp/song.h5 artist_id artist_mbid \
+python preproc/msongsdb/extract_features.py /tmp/${out}.h5 artist_id artist_mbid \
   artist_terms song_id track_7digitalid track_id year danceability duration energy key loudness \
   mode sections_start segments_start segments_loudness_max segments_pitches segments_timbre tempo \
   time_signature > /tmp/${out}.feat
