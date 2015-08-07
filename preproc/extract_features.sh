6343443e4ad282bc9bfc6b73ec29aae58037eaa3
Doug Kang <dougkang@gmail.com>
2015-08-06 17:34:14 -0700

    making extract_features more flexible

--- preproc/extract_features.sh
+++ preproc/extract_features.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-while read line; do
-  out=`echo $line | cut -d/ -f3- | sed 's/\//-/g'`
-  echo "reading $line, writing to /millionSongFeats/$out"
-  /usr/local/hadoop/bin/hdfs dfs -get $line /tmp/song.h5
-  python preproc/msongsdb/extract_features.py /tmp/song.h5 artist_id artist_mbid \
-    artist_terms song_id track_7digitalid track_id year danceability duration energy key loudness \
-    mode sections_start segments_start segments_loudness_max segments_pitches segments_timbre tempo \
-    time_signature > /tmp/song.feat
-  /usr/local/hadoop/bin/hdfs dfs -put /tmp/song.feat /millionSongFeats/$out
-  rm /tmp/song.h5 /tmp/song.feat
-done
-
-echo "completed!"
+line=$1
+out=`echo $line | cut -d/ -f3- | sed 's/\//-/g'`
+echo "reading $line, writing to /millionSongFeats/$out"
+/usr/local/hadoop/bin/hdfs dfs -get $line /tmp/${out}.h5
+echo "extracting features from /tmp/$out"
+python preproc/msongsdb/extract_features.py /tmp/song.h5 artist_id artist_mbid \
+  artist_terms song_id track_7digitalid track_id year danceability duration energy key loudness \
+  mode sections_start segments_start segments_loudness_max segments_pitches segments_timbre tempo \
+  time_signature > /tmp/${out}.feat
+echo "putting /millionSongFeats/$out"
+/usr/local/hadoop/bin/hdfs dfs -put /tmp/${out}.feat /millionSongFeats/$out
+rm /tmp/${out}.h5 /tmp/${out}.feat
