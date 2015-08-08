e3a8a289e28004af18e878a9d96069acd8acc0d9
Doug Kang <dougkang@gmail.com>
2015-08-07 18:50:21 -0700

    auto-download/extract

--- preproc/extract_features.sh
+++ preproc/extract_features.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+cd /tmp
+for x in {A..Z}; do
+  fn="$x.tar.gz"
+  echo "downloading $fn"
+  rsync -avzuP "publicdata.opensciencedatacloud.org::ark:/31807/osdc-c1c763e4/$fn" "/tmp/$fn"
+  tar xzvf $fn
+  echo "extracting features from $x"
+  find $x/ -type f -name *.h5 | \
+    xargs -I{} python preproc/msongsdb/extract_features.py {} artist_id artist_mbid \
+      artist_terms song_id track_7digitalid track_id year danceability duration energy key loudness \
+      mode sections_start segments_start segments_loudness_max segments_pitches segments_timbre tempo \
+      time_signature > $out
+  out="$x.feat"
+  echo "putting /millionSongFeats/$out"
+  /usr/local/hadoop/bin/hdfs dfs -put $out /millionSongFeats/$x
+  echo "cleaning up"
+  rm -r $x $fn $out
+done
+
 line=$1
-out=`echo $line | cut -d/ -f3- | sed 's/\//-/g'`
-echo "reading $line, writing to /millionSongFeats/$out"
-/usr/local/hadoop/bin/hdfs dfs -get $line /tmp/${out}.h5
-echo "extracting features from /tmp/$out"
-python preproc/msongsdb/extract_features.py /tmp/${out}.h5 artist_id artist_mbid \
-  artist_terms song_id track_7digitalid track_id year danceability duration energy key loudness \
-  mode sections_start segments_start segments_loudness_max segments_pitches segments_timbre tempo \
-  time_signature > /tmp/${out}.feat
-echo "putting /millionSongFeats/$out"
-/usr/local/hadoop/bin/hdfs dfs -put /tmp/${out}.feat /millionSongFeats/$out
-rm /tmp/${out}.h5 /tmp/${out}.feat
