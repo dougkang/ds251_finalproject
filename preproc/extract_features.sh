a20286897c00229d23e483d0d19d1570a5d0db72
Doug Kang <dougkang@gmail.com>
2015-08-07 19:50:03 -0700

    fixing issues with extract script

--- preproc/extract_features.sh
+++ preproc/extract_features.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-cd /tmp
-for x in {A..Z}; do
+mkdir /data/tmp
+cd /data/tmp
+while read -r x; do
   fn="$x.tar.gz"
+  out="$x.feat"
   echo "downloading $fn"
-  rsync -avzuP "publicdata.opensciencedatacloud.org::ark:/31807/osdc-c1c763e4/$fn" "/tmp/$fn"
-  tar xzvf $fn
+  # rsync -avzuP "publicdata.opensciencedatacloud.org::ark:/31807/osdc-c1c763e4/$fn" "$fn"
+  # tar xzvf $fn
   echo "extracting features from $x"
-  find $x/ -type f -name *.h5 | \
-    xargs -I{} python preproc/msongsdb/extract_features.py {} artist_id artist_mbid \
+  find . -type f -name *.h5 | \
+    xargs -I{} python /root/ds251_finalproject/preproc/msongsdb/extract_features.py {} artist_id artist_mbid \
       artist_terms song_id track_7digitalid track_id year danceability duration energy key loudness \
       mode sections_start segments_start segments_loudness_max segments_pitches segments_timbre tempo \
       time_signature > $out
-  out="$x.feat"
   echo "putting /millionSongFeats/$out"
   /usr/local/hadoop/bin/hdfs dfs -put $out /millionSongFeats/$x
   echo "cleaning up"
   rm -r $x $fn $out
 done
-
-line=$1
