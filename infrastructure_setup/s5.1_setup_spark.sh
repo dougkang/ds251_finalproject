6b1810cb4a98db420eb3ee7d2c4c42676ce6907b
Taylor Ma <taylorma@berkeley.edu>
2015-08-16 15:16:24 -0400

    update to infrastructure_setup dir

---
+++ infrastructure_setup/s5.1_setup_spark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+salt-ssh '*' "cat << _EOF >> /home/hadoop/.bashrc
+export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/jre"
+export SPARK_HOME="/usr/local/spark"
+export HADOOP_CONF_DIR="/usr/local/hadoop/etc/hadoop"
+export PATH=$SPARK_HOME/bin:$PATH
+export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
+export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$PYTHONPATH
+export PATH=$JAVA_HOME/bin:$PATH"
+salt-ssh '*' "curl http://d3kbcqa49mib13.cloudfront.net/spark-1.4.1-bin-hadoop2.6.tgz | tar -zx -C /usr/local --show-transformed --transform='s,/*[^/]*,spark,'"
+salt-ssh 'master' "cat <<_EOF>> /usr/local/spark/conf/slaves
+master
+slave1
+slave2
+slave3
+slave4"
+
+echo 'Please ssh to the master node and startup the spark cluster by running $SPARK_HOME/sbin/start-all.sh'
