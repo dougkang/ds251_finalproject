c3928f5e36c60f0ce38ae0a5d9676636172db824
Taylor Ma <taylorma@berkeley.edu>
2015-08-16 11:29:26 -0400

    update to setup dir for salt and hadoop

--- setup/s1_provision_vm.sh
+++ setup/s1_provision_vm.sh
@@ -4,3 +4,5 @@ do
     yes | slcli vs create -D w251.final -H ${host} -c 2 -m 4096 -o UBUNTU_LATEST_64 -d dal09 -k key-tma --disk 25 --disk 100 -n 1000
     sleep 10
 done
+
+echo "run slcli vs list to make sure VMs are ready before continuing..."
--- setup/s2_make_hosts_file.sh
+++ setup/s2_make_hosts_file.sh
@@ -7,14 +7,24 @@ do
     esac
 done
 
+if [ -z "${MAGIC_STR}" ]
+then
+    echo "MAGIC_STR cannot be empty"
+    exit 1
+fi
+
 echo "127.0.0.1   localhost.localdomain localhost" > hosts
 slcli vs list | grep ${MAGIC_STR} | awk -F ' ' '{print $4,$2}' >> hosts
 slcli vs list | grep ${MAGIC_STR} | awk -F ' ' '{print $3}' > external_ips
 
+CSSH="cssh "
 for h in $(cat external_ips)
 do
     scp ~/.ssh/id_rsa root@${h}:/root/.ssh
     scp ~/.ssh/id_rsa.pub root@${h}:/root/.ssh
+    scp ./hosts root@${h}:/etc/hosts
+    CSSH="$CSSH root@$h "
 done
 
-
+echo "Please use the following command to cssh to each machine and make sure ssh inter hosts are really passwordless before continuing..."
+echo "$CSSH"
